60c17039885fcbb0bb839d8ff426e89ebb34e2d4
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-25 15:18:57 +0100

    Products array ready

--- data/products.php
+++ data/products.php
@@ -2,12 +2,15 @@
 
 require_once __DIR__ . "/../models/product.php";
 require_once __DIR__ . "/../models/product.php";
+require_once __DIR__ . "/../models/bed.php";
+require_once __DIR__ . "/../models/food.php";
+require_once __DIR__ . "/../models/toy.php";
 
 $products = [
-   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
-   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
-   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
-   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
-   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
-   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50)
+    new Bed("Ragnarok", new Category("Dog"), 234, "Big size doggo bed", 34000, "Big", "Cotton"),
+    new Bed("Caragor", new Category("Cat"), 2, "LOTR themed catto bed", 16000, "Medium", "Dragon scales"),
+    new Food("Chicken nuggets", new Category("Dog"), 13000, "Delicious nuggets", 80.99, "Extra large", "Chicken"),
+    new Food("Big Tuna", new Category("Cat"), 45000, "Savage tasty tuna", 149.99, "Small", "Tuna"),
+    new Toy("Mega ball", new Category("Dog"), 187, "High speed mega ball", 99.99, "Yellow", "Plastic"),
+    new Toy("Torpedo rat", new Category("Cat"), 82, "Torpedo shaped rat", 1500, "Grey", "Rubber")
 ];
