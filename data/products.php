7a9d477776a2c502ccb0714732b3b6264ba16fd1
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-25 14:42:39 +0100

    Var dump test print ok

--- data/products.php
+++ data/products.php
@@ -1,5 +1,13 @@
 <?php
 
-$products = [
+require_once __DIR__ . "/../models/product.php";
+require_once __DIR__ . "/../models/product.php";
 
-]
+$products = [
+   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
+   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
+   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
+   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
+   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50),
+   new Product("pollo", new Category("Dog"), 6, "Pollo gommoso", 10,50)
+];
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once __DIR__ . "/data/products.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +15,7 @@
 </head>
 
 <body>
-
+    <?php var_dump($products) ?>
 </body>
 
 </html>
