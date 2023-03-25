5000183425a8f4bb47d42e17dce6d427e8772dba
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-25 15:54:44 +0100

    Early data print

--- data/products.php
+++ data/products.php
@@ -6,11 +6,15 @@ require_once __DIR__ . "/../models/bed.php";
 require_once __DIR__ . "/../models/food.php";
 require_once __DIR__ . "/../models/toy.php";
 
-$products = [
+$beds = [
     new Bed("Ragnarok", new Category("Dog"), 234, "Big size doggo bed", 34000, "Big", "Cotton"),
     new Bed("Caragor", new Category("Cat"), 2, "LOTR themed catto bed", 16000, "Medium", "Dragon scales"),
+];
+$food = [
     new Food("Chicken nuggets", new Category("Dog"), 13000, "Delicious nuggets", 80.99, "Extra large", "Chicken"),
     new Food("Big Tuna", new Category("Cat"), 45000, "Savage tasty tuna", 149.99, "Small", "Tuna"),
+];
+$toys = [
     new Toy("Mega ball", new Category("Dog"), 187, "High speed mega ball", 99.99, "Yellow", "Plastic"),
     new Toy("Torpedo rat", new Category("Cat"), 82, "Torpedo shaped rat", 1500, "Grey", "Rubber")
 ];
--- index.php
+++ index.php
@@ -8,14 +8,73 @@ include_once __DIR__ . "/data/products.php";
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP Extends</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <?php var_dump($products) ?>
+
+    <div class="container">
+        <h1>PRODUCTS</h1>
+        <div class="row">
+            <?php foreach ($beds as $bed) : ?>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $bed->name ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?= $bed->name ?></h6>
+                        <p class="card-text"><?= $bed->$availability ?></p>
+                        <p><?= $bed->description ?></p>
+                        <p><?= $bed->$price ?></p>
+                        <p><?= $bed->size ?></p>
+                        <p><?= $bed->material ?></p>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach ?>
+        </div>
+        <div class="row">
+            <?php foreach ($food as $food) : ?>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $food->name ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?= $food->name ?></h6>
+                        <p class="card-text"><?= $food->$availability ?></p>
+                        <p><?= $food->description ?></p>
+                        <p><?= $food->$price ?></p>
+                        <p><?= $food->size ?></p>
+                        <p><?= $food->main_ingredient ?></p>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach ?>
+        </div>
+        <div class="row">
+            <?php foreach ($toys as $toy) : ?>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $toy->name ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?= $toy->name ?></h6>
+                        <p class="card-text"><?= $toy->$availability ?></p>
+                        <p><?= $toy->description ?></p>
+                        <p><?= $toy->$price ?></p>
+                        <p><?= $toy->color ?></p>
+                        <p><?= $toy->material ?></p>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach ?>
+        </div>
+    </div>
 </body>
 
 </html>
